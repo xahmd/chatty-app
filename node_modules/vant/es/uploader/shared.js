75f0aa2cdf27b4546339ca93e9a49f954122280d
ahmedmohja <hfl@outlook.sa>
2021-05-23 14:57:55 +0300

    First Committ

--- node_modules/vant/es/uploader/shared.js
+++
@@ -1,7 +0,0 @@
-import { createNamespace } from '../utils';
-
-var _createNamespace = createNamespace('uploader'),
-    createComponent = _createNamespace[0],
-    bem = _createNamespace[1];
-
-export { bem, createComponent };
