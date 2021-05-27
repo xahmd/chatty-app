d63e897cfece9e77644099e099b17e6a42a92b8b
ahmedmohja <hfl@outlook.sa>
2021-05-27 13:28:20 +0300

    First Committ

---
+++ src/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="card text-center">
+      <img
+        src="~assets/logo.png"
+        style="height:auto;width:100px;"
+      >
+      <form action="" @submit.prevent="click()">
+        <q-input ref="username" dark dir="rtl" v-model="username" placeholder="الاسم" :rules="[ username => username.length > 2 || 'الرجاء وضع اسم لايقل عن ثلاث خانات']" />
+        <q-input ref="idroom" type="number" dark dir="rtl" v-model="room" placeholder="رقم الغرفة" :rules="[val => !!val || 'يجب وضع رقم الغرفة']"/>
+        <br>
+        <q-btn @click="click" push color="primary" type="submit" text-color="white" class="full-width" label="دخول" />
+      </form>
+      <p style="color:white;margin-top:10px;">بمجرد دخولك فإنك توافق على <a style="color:#43B3C0;text-decoration: blink;" href="#/tos">سياسة الاستخدام</a></p>
+    </div>
+    <q-footer class="text-white text-center" style="background-color: #1E1E1E;">
+      <q-toolbar>
+        <q-toolbar-title>
+          <p style="font-size: 15px;">Made With ❤️ By <a style="color:#43B3C0;text-decoration: blink;" href="https://github.com/ahmedbinmoh">Ahmed</a> & <a style="color:#43B3C0;text-decoration: blink;" href="https://instagram.com/alonemazin">Mazin</a></p>
+        </q-toolbar-title>
+      </q-toolbar>
+  </q-footer>
+  </q-page>
+</template>
+
+<script>
+import { LocalStorage } from 'quasar'
+
+export default {
+  data(){
+    return{
+      username:"",
+      room:""
+    }
+  },
+  methods: {
+    click(){
+      this.$refs.username.validate()
+      this.$refs.idroom.validate()
+      if(this.$refs.username.validate() == true && this.$refs.idroom.validate() == true){
+        LocalStorage.set("username", this.username)
+        this.$router.push("/chat/"+ this.room)
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/chat.vue
@@ -0,0 +1,144 @@
+<template>
+    <q-page class="text-center">
+        <q-header elevated class="text-white flex flex-center" style="background-color: #181818;">
+            <img
+                src="~assets/logo.png"
+                style="height:auto;width:60px;margin-right:auto;margin-left:25px;"
+            >
+            <a style="margin-right:auto;">رقم الغرفة: {{this.roomid}}</a>
+            <q-btn flat color="primary" label="الصفحة الرئيسية" v-go-back=" '/' " style="margin-right:10px;"/>
+        </q-header>
+
+        <div class="q-pa-md row justify-center">
+            <div style="width: 100%;">
+                <div v-for="Message in messages">
+                    <q-chat-message 
+                        style="color: white; text-align: right;"
+                        v-if="Message.username == usernameIndex"
+                        :name="[Message.username]"
+                        text-color="white"
+                        bg-color="secondary"
+                        :text="[sanitizeString(Message.content.toString())]"
+                        sent              
+                    />
+                    <q-chat-message 
+                        style="color: white; text-align: left;"
+                        v-else
+                        :name="[Message.username]"
+                        text-color="white"
+                        bg-color="primary"
+                        :text="[sanitizeString(Message.content.toString())]"
+                    />
+                </div>
+            </div>
+        </div>
+        
+        <q-footer class="text-white" style="background-color: #181818;">
+        <q-toolbar>
+            <q-toolbar-title>
+                <form action="" @submit.prevent="SendMessage()">
+                    <q-input dark bottom-slots v-model="newMessage" placeholder="أكتب رسالتك هنا" dir="rtl" maxlength="200">
+                        <template v-slot:append>
+                        </template>
+
+                        <template v-slot:after>
+                        <q-btn round @click="SendMessage()" dense flat icon="send" />
+                        </template>
+                    </q-input>
+                </form>
+            </q-toolbar-title>
+        </q-toolbar>
+        </q-footer>
+    </q-page>
+</template>
+
+<script>
+import { LocalStorage } from 'quasar'
+import db from './db';
+
+export default {
+    data(){
+        return{
+            usernameIndex: LocalStorage.getItem('username'),
+            newMessage: "",
+            roomid: "",
+            messages: []
+        }
+    },
+    methods: {
+    scrollToBottom() {
+      setTimeout(() => {
+        window.scrollTo({
+          left: 0,
+          top:
+            document.body.scrollHeight || document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      }, 50);
+    },
+    loadMessages() {
+      const messageRef = db.database().ref(`messages-${this.roomid}`);
+      messageRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        if (data == null || data == undefined) {
+          return;
+        }
+        let messages = [];
+        const keys = Object.keys(snapshot.val());
+        for (let i = 0; i < keys.length; i++) {
+          const key = keys[i];
+
+          
+          const displayTime =
+            i === 0
+              ? true
+              : data[keys[i]].time - data[keys[i - 1]].time > 300000;
+
+          messages.push({
+            id: key,
+            username: data[key].username,
+            content: data[key].content,
+            time: data[key].time,
+            displayTime: displayTime,
+          });
+        }
+
+        this.messages = messages;
+
+        this.scrollToBottom();
+      });
+    },
+    SendMessage() {
+      let messageRef = db.database().ref(`messages-${this.roomid}`);
+      if (this.newMessage === '' || this.newMessage === null) {
+        return;
+      }
+
+      let message = {
+        username: this.usernameIndex,
+        content: this.newMessage,
+        time: Date.now(),
+      };
+
+      messageRef.push(message);
+      this.newMessage = '';
+    },
+    sanitizeString(str) {
+    const patterns = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      "`": '&grave;'
+    };
+    const reg = /[&<>"']/ig;
+    return str.replace(reg, (match)=>(patterns[match]));
+  }
+    },
+        mounted(){
+        this.roomid = this.$route.params.roomid
+        this.loadMessages()
+    }
+}
+</script>
